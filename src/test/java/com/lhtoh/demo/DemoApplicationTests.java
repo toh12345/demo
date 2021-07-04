0fd7adc8cd08afb5d94cfc24167416ad4a8faa3d
LIHOONG0511 <tlh_5012@hotmail.com>
2021-07-04 23:36:31 +0800

    first commit

---
+++ src/test/java/com/lhtoh/demo/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.lhtoh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test");
+	}
+
+	void main(){
+		contextLoads();
+		
+	}
+
+}
